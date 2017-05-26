f8addad8d0c113d5108ccf17ea5c87ffc08f2ed7
JeongJu Kim <haje01@gmail.com>
2017-05-26 18:06:08 +0900

    copy config

--- build.bat
+++ build.bat
@@ -2,5 +2,6 @@ dist\conmon.exe stop
 dist\conmon.exe remove
 pyinstaller -F --hidden-import=win32timezone conmon.py
 copy _config.yml dist\
+copy config.yml dist\
 dist\conmon.exe --startup=auto install
 dist\conmon.exe start
